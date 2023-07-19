7e76efcc1b8bf3f1f29f0ea9f2f3fc09f7607af4
Trippy009 <oriekaosecollins@yahoo.com>
2023-07-19 08:26:22 -0700

    11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * print_10_to_98 - Prints all natural numbers from n to 98
+ * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
- * Return: always 0
+ * Return: Always 0
  */
 
 void print_to_98(int n)
