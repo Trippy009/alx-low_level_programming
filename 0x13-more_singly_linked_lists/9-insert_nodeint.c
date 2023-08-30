24a177b4604af128cbd1d16597a0366a7b67da5b
Trippy009 <oriekaosecollins@yahoo.com>
2023-08-29 18:57:33 -0700

    trying to fix up errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a new node inserted in a linked list,
+ * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
- * @n: insert data in the new node
+ * @n: data to insert in the new node
  *
  * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int o;
+	unsigned int i;
 	listint_t *new;
 	listint_t *temp = *head;
 
@@ -29,9 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (o = 0; temp && o < idx; o++)
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (o == idx - 1)
+		if (i == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;
